0ece7fe233a5fc35345e874b9b8baa8e3a31a401
mohdqabbani <mohammad.qabbani.mq@gmail.com>
2020-02-12 21:12:16 +0200

    Updates

---
+++ precache-manifest.7e49b4b9b0c3740e3486342bf0981130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6de59ccb7b74fbc85b10cadd02af2c8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "23846916037baccedb89",
+    "url": "/monsters/static/css/main.2fbbfa81.chunk.css"
+  },
+  {
+    "revision": "204c65eb67574eada0eb",
+    "url": "/monsters/static/js/2.e499a9cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.e499a9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23846916037baccedb89",
+    "url": "/monsters/static/js/main.9ae413c5.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
